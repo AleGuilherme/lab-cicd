20e3c7718186547c21bad3ee8fae8ee13e718977
Alexandre <alliux1977@gmail.com>
2020-02-08 22:09:54 -0300

    [FIX 5]

--- Jenkinsfile
+++ Jenkinsfile
@@ -3,12 +3,7 @@ pipeline {
 
     stages {
         stage('Build') {
-	    agent {
-                docker { image 'maven:3-alpine' }
-            }
             steps {
-                 sh 'mvn -B -DskipTests clean package'
-		 archiveArtifacts 'target/*.jar'
             }
         }
     }
---
+++ hello.py
@@ -0,0 +1,9 @@
+from wsgiref.simple_server import make_server
+#print('Hello, world!')
+GREETING = b'Hello, world!\n'
+#
+def hello(environ, start_response):
+        start_response('200 OK', [('Content-Type', 'text/plain')])
+        return [GREETING]
+
+make_server('0.0.0.0', 9000, hello).serve_forever()
