5ae1d37cf41ee2d6add5c13d9ec2b299c8ae364f
AleGuilherme <alliux@hotmail.com>
2020-03-18 23:59:16 -0300

    [Change] green

--- app/GREEN/hello.py
+++ app/GREEN/hello.py
@@ -1,6 +1,6 @@
 from wsgiref.simple_server import make_server
 #print('Hello, world!')
-GREETING = b'Hello, world!\n (version 2)'
+GREETING = b'Hello, GREEN!\n (version 2)'
 #
 def hello(environ, start_response):
         start_response('200 OK', [('Content-Type', 'text/plain')])
